00b1fc3c9b9a013bc4e4875a1a0520a060115c3d
“Keren <kerencharles4@gmail.com>
2022-01-31 17:03:08 -0500

    bc-library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 const express = require("express")
+const { initializeApp, cert } = require("firebase-admin/app")
+const { getFirestore } = require("firebase-admin/firestore")
+
+const credentials = require("../credentials.json")
+
+initializeApp({
+  credential: cert(credentials),
+})
+
+const db = getFirestore()
 
 const app = express()
 app.use(express.json())
 
 app.get("/", (request, response) => {
-  response.send("Hello Keren")
+  const usersRef = db.collection("users")
+  usersRef.get().then((snapshot) => {
+    const users = []
+    snapshot.forEach((doc) => {
+      users.push({
+        id: doc.id,
+        ...doc.data(),
+      })
+    })
+    response.send(users)
+  })
+  //response.send('Hello World!')
 })
 
 app.post("/users", (req, res) => {
@@ -12,7 +33,7 @@ app.post("/users", (req, res) => {
 
   const user = { name, age, email }
 
-  const result = `My name is ${user.name}, I am ${user.age} years old my email is ${user.email}`
+  const result = `My name is ${user.name}, I am ${user.age} years old and my email is ${user.email}`
 
   res.send(result)
 })
